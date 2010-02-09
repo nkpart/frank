e836047b11f62a4f55dbaa46503da958b75b082f
Nick Partridge <nkpart@gmail.com>
2010-02-10 09:16:41 +1000

    Views working

--- src/main/scala/frank/examples/beers.scala
+++ src/main/scala/frank/examples/beers.scala
@@ -1,24 +1,34 @@
-// package frank
-// package examples
-// 
-// import belt._
-// 
-// object root extends Belt {
-//   def service(r: Request): Response = {
-//     frank_!(r)
-//     
-//     r :>> {
-//       
-//     }
-//   }
-// }
-// 
-// class start extends root {
-//   at("/beers/:id") { request =>
-//     val id = request("id")
-//     
-//     
-//   }
-// }
-// 
-// 
+
+package belt {
+  class Application extends belt.Belt {
+    def service(r: Request): Response = {
+      val beers: frank.RestBase = new frank.examples.Beers(r)
+      beers.apply(r)
+    }
+  }
+}
+
+package frank {
+package examples {
+
+import scala.xml._
+import belt._
+import rest._
+
+class Beers(request: Request) extends RestBase {
+  rest("breweries", Show).view(someView) { id =>
+    (OK, id.toInt)
+  }
+  
+  layout { body =>
+    <html>
+      { body }
+    </html>
+  }
+  
+  val someView: Int => NodeSeq = (id: Int) => {
+    <p>got id: {id}</p>
+  }
+}
+
+}}
--- src/main/scala/frank/frank.scala
+++ src/main/scala/frank/frank.scala
@@ -1,43 +1,45 @@
 package frank
 
+import scala.xml._
 import scalaz.{Show => _, Index => _, _}
 import Scalaz._
 import rest._
 import belt._
 import scalaz.http.response.{Response => _, _}
+import scalaz.http.Slinky._
 
 import scala.collection.mutable.{Map => MMap}
 import scala.collection.mutable.ArrayBuffer
 
-object run extends Belt {
-  def symbolFor[T](a: Action[T]) = a match {
-    case New => 'new
-    case Create => 'create
-    case Edit(_) => 'edit
-    case Index => 'index
-    case Show(_) => 'show
-    case Destroy(_) => 'destroy
-    case Update(_) => 'update
+trait RestBase {
+  implicit val charset = UTF8
+  
+  var layoutVar: Option[(NodeSeq => NodeSeq)] = none
+  
+  def layout(f: NodeSeq => NodeSeq) {
+    layoutVar = some(f)
   }
   
-  def service(r: Request): Response = {
-    MyApp(r)
-  }
-}
-
-trait RenderArgs {
-  val renderArgs: MMap[String, Any] = MMap()
-
-  implicit def pimpedString(s: String) = new {
-    def <+(a: Any) = renderArgs += (s -> a)
-  }
-}
-
-trait RestBase {
   val actions: ArrayBuffer[Request => Option[belt.Response]] = new ArrayBuffer
   
-  def rest(resource: String, s: Show.type)(f: String => Response) = {
-    actions += (r => r.action match { case Some((r, Show(id))) if r == resource => some(f(id)); case _ => none })
+  def rewrite(pf: PartialFunction[Request, Action[String]]) = pf
+  
+  def rest[T](resource: String, s: Show.type) = new {
+    def view(v: T => NodeSeq)(action: String => (Status, T)) = {
+      actions += (r => r.action match { 
+        case Some((r, Show(id))) if r == resource => some {
+          val actionResult: (Status, T) = action(id)
+          val viewResult: NodeSeq = v(actionResult._2)
+          val layedOutValue: NodeSeq = layoutVar map (f => f(viewResult)) getOrElse viewResult
+          Response(actionResult._1)(r => r << layedOutValue)
+        }
+        case _ => none
+      })
+    }
+  }
+  
+  def rest(resource: String, i: Index.type)(f: => Response) = {
+    actions += (r => r.action match { case Some((r, Index)) if r == resource => some(f); case _ => none })
   }
   
   def apply(r: Request) = {
@@ -45,16 +47,5 @@ trait RestBase {
       Response(NotFound)
     }
   }
-//  def rest(a: New.type)(r: => Response) = action('new){case New => r}
 }
 
-object MyApp extends RenderArgs with RestBase {
-  rest("breweries", Show) { id =>
-    Response(OK)
-  }
-  
-  // action('show) { case Show(id) =>
-  //   "foo" <+ id
-  //   belt.Response(OK)
-  // }
-}
